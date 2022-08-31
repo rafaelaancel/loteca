9e7fa1e87865db2cfdb36b5f9439ab7b352c9646
rafaelaancel <rafaelancel@hotmail.com>
2022-08-31 09:05:48 -0400

    .

---
+++ funcoes.js
@@ -0,0 +1,39 @@
+var numSort = [] 
+var numEsco = []
+
+function sortNumeros() {
+
+for(let i = 0; i < 6; i++) {
+
+let num = Math.ceil (Math.random() * 60);
+console.log(num);
+if(!numSort.includes(num)) {
+    numSort[i] = num;    
+} else {
+ i = i - 1;   
+}
+
+}
+
+console.log(numSort)
+
+}
+
+function lerNumero(value, pos) {
+if(value < 1) {
+    alert("Número não pode ser zero ou negativo! Digite novamente.")
+} else if (value > 60) {
+    alert("Número não pode ser maior do que 60! Digite novamente.")
+ } else if(numEsco.includes(value)) {
+    alert("Número repetido! Digite novamente.")
+ } else {
+    
+numEsco[pos] = value
+console.log(numEsco)
+
+}
+}
+
+function verAcertos() {
+    
+}
