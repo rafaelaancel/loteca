2e55ab419a402fd1dbc051c9b79c8076a3192b68
rafaelaancel <rafaelancel@hotmail.com>
2022-09-14 09:16:23 -0400

    naoseiiii

--- funcoes.js
+++ funcoes.js
@@ -1,52 +1,44 @@
-var numSort = [] 
-var numEsco = []
+var numEsco = [];
+var numSort = [];
 
 function sortNumeros() {
+  for (let i = 0; i < 6; i++) {
+    let num = Math.ceil(Math.random() * 60);
+    console.log(num);
+    if (!numSort.includes(num)) {
+      numSort[i] = num;
+    } else {
+      i = i - 1;
+    }
+  }
 
-for(let i = 0; i < 6; i++) {
-
-let num = Math.ceil (Math.random() * 60);
-console.log(num);
-if(!numSort.includes(num)) {
-    numSort[i] = num;    
-} else {
- i = i - 1;   
+  console.log(numSort)
+  document.getElementById("sorteados").innerHTML = numSort
+  contarAcertos()
 }
 
-}
-
-console.log(numSort)
-document.getElementById("sorteados").innerHTML = numSort()
-contarAcertos()
-
-}
-
-function lerNumero(value, pos) {
-if(value < 1) {
-    alert("Número não pode ser zero ou negativo! Digite novamente.")
-} else if (value > 60) {
-    alert("Número não pode ser maior do que 60! Digite novamente.")
- } else if(numEsco.includes(value)) {
-    alert("Número repetido! Digite novamente.")
- } else {
-    
-numEsco[pos] = value
-console.log(numEsco)
+function lerNumero(value, pos){
+  let num = Number(value)
+  if(num < 1) {
+    alert("Numero não pode ser zero ou negativo! Digite novamente")
+  } else if(num > 60) {
+    alert("Numero não pode ser maior que 60! Digite novamente")
+  } else if(numEsco.includes(num)){
+    alert("Numero Repetido! Digite novamente")
+  } else {
+    numEsco[pos] = num
+    console.log(numEsco)
+  }
 
 }
-}
 
-function verAcertos() {
-    
-}
+function contarAcertos(){
+  let cont = 0;
+  numEsco.forEach(function(value,index){
+    if(numSort.includes(value)){
+      cont = cont + 1;
+    }
+  })
+  document.getElementById("acertos").innerHTML = cont;
 
-function contarAcertos() {
-    let cont = 0;
-    numEsco.forEach(function(value, index){
-        if (numSort.includes(value)) {
-            cont++;
-        }
-    } )
-
-    console.log(cont)
 }
